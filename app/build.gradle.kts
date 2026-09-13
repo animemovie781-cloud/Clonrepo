3005e556e138924d2a77b80ff8ae526a141d7de2
animemovie781-cloud <animemovie781@gmail.com>
2026-09-13 21:58:53 +0530

    build: enable R8 shrinking and obfuscation
    Enable code minification and resource shrinking for release builds to reduce APK size and improve security.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             lint {
                 checkReleaseBuilds = false
             }
